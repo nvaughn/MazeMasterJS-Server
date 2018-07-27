a4727ec94d866f8964555bfa6845ea3d9020e1c5
James Dean <kreebog@gmail.com>
2018-07-27 16:22:52 -0400

    updating packages - selected NeDB (embedded database) for testing.

--- bin/src/server.js
+++ bin/src/server.js
@@ -1,19 +1,14 @@
-import fs from 'fs';
-import path from 'path';
-import { format as fmt } from 'util';
-import { Logger, LOG_LEVELS } from '../lib/Logger';
-
-const log = Logger.getInstance();
-log.setLogLevel(LOG_LEVELS.DEBUG);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Logger_1 = require("../lib/Logger");
+const log = Logger_1.Logger.getInstance();
+log.setLogLevel(Logger_1.LOG_LEVELS.DEBUG);
 log.appInfo(__filename, '');
-
 // start up the server
 startServer();
-
 function startServer() {
     log.info(__filename, 'startServer()', 'Server started.');
 }
-
 /**
  * Watch for SIGINT (process interrupt signal) and trigger shutdown
  */
@@ -22,7 +17,6 @@ process.on('SIGINT', function onSigInt() {
     log.info(__filename, 'onSigInt()', 'Got SIGINT - Exiting application...');
     doShutdown();
 });
-
 /**
  * Watch for SIGTERM (process terminate signal) and trigger shutdown
  */
@@ -31,10 +25,10 @@ process.on('SIGTERM', function onSigTerm() {
     log.info(__filename, 'onSigTerm()', 'Got SIGTERM - Exiting application...');
     doShutdown();
 });
-
 /**
  * Gracefully shut down the service
  */
 function doShutdown() {
     log.info(__filename, 'doShutDown()', 'Closing HTTP Server connections...');
 }
+//# sourceMappingURL=server.js.map
